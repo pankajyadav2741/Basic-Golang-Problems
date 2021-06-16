1388afbe99ca38e05e43222a80f5721b78cde806
Pankaj Yadav <pankajyadav2741@gmail.com>
2021-06-16 20:33:44 +0530

    Update main.go
    Added code logic.

--- Arrays/1. Reverse Array/main.go
+++ Arrays/1. Reverse Array/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 func main() {
-  
+	arr := [5]int {1,2,3,4,5}
+	arrLen := len(arr)
+	fmt.Println("Before Reversal:", arr)
+	for i:=0; i<arrLen/2; i++ {
+		arr[i], arr[arrLen-i-1] = arr[arrLen-i-1], arr[i]
+	}
+	fmt.Println("After Reversal:", arr)
 }
