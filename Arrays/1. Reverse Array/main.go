06b897a4d7de112f0674bc0ccf111e6f26102403
Pankaj Yadav <pankajyadav2741@gmail.com>
2021-06-17 10:26:01 +0530

    Update main.go
    Refined code.

--- Arrays/1. Reverse Array/main.go
+++ Arrays/1. Reverse Array/main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 )
 
-func main() {
-	arr := [5]int {1,2,3,4,5}
+func reverse(arr []int) []int {
 	arrLen := len(arr)
-	fmt.Println("Before Reversal:", arr)
 	for i:=0; i<arrLen/2; i++ {
 		arr[i], arr[arrLen-i-1] = arr[arrLen-i-1], arr[i]
 	}
-	fmt.Println("After Reversal:", arr)
+	return arr
+}
+
+func main() {
+	arr := []int {1,2,3,4,5}
+	fmt.Println("Before Reversal: ", arr)
+	arr = reverse(arr)
+	fmt.Println("After Reversal: ", arr)
 }
