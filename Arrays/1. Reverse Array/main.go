ee5dc05a5adc87bd2c00f422ca22385f7c2f1324
Yadav <pankaj.1.yadav@nokia.com>
2021-06-17 10:53:12 +0530

    Added Unit Tests

--- Arrays/1. Reverse Array/main.go
+++ Arrays/1. Reverse Array/main.go
@@ -6,14 +6,14 @@ import (
 
 func reverse(arr []int) []int {
 	arrLen := len(arr)
-	for i:=0; i<arrLen/2; i++ {
+	for i := 0; i < arrLen/2; i++ {
 		arr[i], arr[arrLen-i-1] = arr[arrLen-i-1], arr[i]
 	}
 	return arr
 }
 
 func main() {
-	arr := []int {1,2,3,4,5}
+	arr := []int{1, 2, 3, 4, 5}
 	fmt.Println("Before Reversal: ", arr)
 	arr = reverse(arr)
 	fmt.Println("After Reversal: ", arr)
